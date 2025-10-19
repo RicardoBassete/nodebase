9e2d264d0241c90ccd0ea2bd4670b92093178f7e
Ricardo Bassete <ricardogbassete@gmail.com>
2025-10-19 12:07:34 -0300

    add auth-layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { AuthLayout } from '@/features/auth/components/auth-layout'
 import React from 'react'
 
 interface Props {
@@ -7,23 +6,5 @@ interface Props {
 }
 
 export default function Layout(props: Props) {
-  return (
-    <div className="bg-muted min-h-svh flex flex-col justify-center items-center gap-6 p-6 md:p-10">
-      <div className="w-full max-w-sm flex flex-col gap-6">
-        <Link
-          href="/"
-          className="flex items-center gap-2 self-center font-medium"
-        >
-          <Image
-            src={'/logos/logo.svg'}
-            alt="Nodebase"
-            width={30}
-            height={30}
-          />
-          Nodebase
-        </Link>
-        {props.children}
-      </div>
-    </div>
-  )
+  return <AuthLayout>{props.children}</AuthLayout>
 }
