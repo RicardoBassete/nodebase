fedc39d3e494b510819206933ce1118b3b43befa
Ricardo Bassete <ricardogbassete@gmail.com>
2025-10-19 00:19:22 -0300

    setup better-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-import { getQueryClient, trpc } from '@/trpc/server'
-import { Client } from './client'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
-import { Suspense } from 'react'
+import { requireAuth } from '@/lib/auth-utils'
 
 export default async function Home() {
-  const queryClient = getQueryClient()
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+  await requireAuth()
 
   return (
     <div className="min-h-screen min-w-screen flex flex-col justify-center items-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={'Loading...'}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+      protected by server
     </div>
   )
 }
