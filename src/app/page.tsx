6d6c43b02ebbb79c8e1cad30c93128239bda14ba
Ricardo Bassete <ricardogbassete@gmail.com>
2025-10-19 14:49:03 -0300

    add background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import { requireAuth } from '@/lib/auth-utils'
+'use client'
 
-export default async function Home() {
-  await requireAuth()
+import { Button } from '@/components/ui/button'
+import { useTRPC } from '@/trpc/client'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
+
+export default function Home() {
+  const queryClient = useQueryClient()
+  const trpc = useTRPC()
+  const { data } = useQuery(trpc.getWorkflows.queryOptions())
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success('Job queued')
+      }
+    })
+  )
 
   return (
     <div className="min-h-screen min-w-screen flex flex-col justify-center items-center">
-      protected by server
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
+      {JSON.stringify(data, null, 2)}
     </div>
   )
 }
