b5425bef75da4bc91a1f68cac0f6edae43e2ed38
Ricardo Bassete <ricardogbassete@gmail.com>
2026-01-02 14:59:53 -0300

    setup node selector

--- src/features/editor/components/add-node-button.tsx
+++ src/features/editor/components/add-node-button.tsx
@@ -1,19 +1,24 @@
 'use client'
 
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { PlusIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { NodeSelector } from '@/components/node-selector'
 
 export const AddNodeButton = memo(() => {
+  const [selectorOpen, setSelectorOpen] = useState(false)
+
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={() => {}}
-      className="bg-background"
-    >
-      <PlusIcon className="size-4" />
-    </Button>
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => {}}
+        className="bg-background"
+      >
+        <PlusIcon className="size-4" />
+      </Button>
+    </NodeSelector>
   )
 })
 
