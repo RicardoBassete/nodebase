a9af71e12b43ab81fde6a1ac311390c3ad4f0f0a
Ricardo Bassete <ricardogbassete@gmail.com>
2026-01-03 12:21:59 -0300

    add manual trigger config dialog

--- src/features/triggers/components/manual-trigger/node.tsx
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -2,20 +2,28 @@
 
 import type { NodeProps, Node } from '@xyflow/react'
 import { GlobeIcon, MousePointerIcon } from 'lucide-react'
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { BaseTriggerNode } from '../base-trigger-node'
+import { ManualTriggerDialog } from './dialog'
 
 export const ManualTriggerNode = memo((props: NodeProps) => {
+  const [dialogOpen, setDialogOpen] = useState(false)
+
+  const handleOpenSettings = () => {
+    setDialogOpen(true)
+  }
+
   return (
     <>
+      <ManualTriggerDialog open={dialogOpen} onOpenChange={setDialogOpen} />
       <BaseTriggerNode
         {...props}
         icon={MousePointerIcon}
         name="Manual Trigger"
         // TODO
         // status={nodeStatus }
-        // onSettings={handleOpenSettings }
-        // onDoubleClick={handleOpenSettings }
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   )
